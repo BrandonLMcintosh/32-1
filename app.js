66842d74c24e99718eb5a9edb08a32dce3ae3cee
Brandon McIntosh <brandon.l.mcintosh@gmail.com>
2021-04-14 17:38:54 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const Nums = require("./nums");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/mean", (req, res) => {
+	const nums = req.query.nums;
+	if (nums) {
+	}
+	res.send(req.query);
+});
+
+app.get("/median", (req, res) => {});
+
+app.get("/mode", (req, res) => {});
+
+app.listen(5000, () => {
+	console.log("App on port 5000");
+});
---
+++ nums.js
@@ -0,0 +1,41 @@
+const AppError = require("./appError");
+
+class Nums {
+	constructor(req) {
+		this.req = req;
+		this.arr = parse(this.req);
+		this.mean = this._mean(this.arr);
+		this.median = this._median(this.arr);
+		this.mode = this._mode(this.arr);
+	}
+
+	parse = (req) => {
+		try {
+			const numsStringArray = req.query.nums.split(",");
+			if (!numStrings) {
+				throw new AppError('Error: query "nums" must be present');
+			}
+
+			numsIntString = numsStringArray.map((numString) => {
+				numInt = parseInt(numString);
+				if (numInt) {
+					throw new AppError("Error: one or more of the numbers in nums cannot be parsed to an integer");
+				} else {
+					return numInt;
+				}
+			});
+
+			return numStrings;
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	_mean = (arr) => {};
+
+	_median = (arr) => {};
+
+	_mode = (arr) => {};
+}
+
+module.exports = { Nums };
