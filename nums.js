3484052c2d1ff200364a29ad6e7475126fa78d21
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2021-04-21 22:34:05 -0700

    finished functionality

--- nums.js
+++ nums.js
@@ -1,44 +1,65 @@
-const AppError = require("./appError");
+const { AppError } = require("./appError");
 
 class Nums {
-	constructor(req) {
-		this.req = req;
-		this.arr = parse(this.req);
-		this.mean = this._mean(this.arr);
-		this.median = this._median(this.arr);
-		this.mode = this._mode(this.arr);
+	constructor(arr) {
+		this.arr = arr;
+		this.mean = this.calcMean(this.arr);
+		this.median = this.calcMedian(this.arr);
+		this.mode = this.calcMode(this.arr);
 	}
 
-	parse = (req) => {
-		try {
-			const numsStringArray = req.query.nums.split(",");
-			if (!numStrings) {
-				throw new AppError('Error: query "nums" must be present', 400);
+	calcMean(arr) {
+		const total = arr.reduce((acc, val) => {
+			return acc + val;
+		});
+		const mean = parseFloat((total / arr.length).toFixed(4));
+		return mean;
+	}
+
+	calcMedian(arr) {
+		arr.sort((a, b) => {
+			return a - b;
+		});
+		const half = Math.floor(arr.length / 2);
+		if (arr.length % 2) return arr[half];
+		return Math.round(((arr[half - 1] + arr[half]) / 2.0) * 1e4);
+	}
+
+	calcMode(arr) {
+		const sorted = arr.sort();
+		const numMap = {};
+		let bestFrequency = 0;
+		let mode;
+		sorted.forEach((num) => {
+			numMap[num] = (numMap[num] || 0) + 1;
+			if (bestFrequency < numMap[num]) {
+				bestFrequency = numMap[num];
+				mode = num;
 			}
-
-			numsIntString = numsStringArray.map((numString) => {
-				numInt = parseInt(numString);
-				if (numInt) {
-					throw new AppError(
-						"Error: one or more of the numbers in nums cannot be parsed to an integer",
-						400
-					);
-				} else {
-					return numInt;
-				}
-			});
-
-			return numStrings;
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	_mean = (arr) => {};
-
-	_median = (arr) => {};
-
-	_mode = (arr) => {};
+		});
+		return mode;
+	}
 }
 
-module.exports = { Nums };
+parse = (arr) => {
+	const numsIntsArray = arr.map((numString) => {
+		const numInt = parseInt(numString);
+		if (!numInt) {
+			throw new AppError("Error: one or more of the numbers in nums cannot be parsed to an integer", 400);
+		} else {
+			return numInt;
+		}
+	});
+
+	return numsIntsArray;
+};
+
+getArray = (req) => {
+	const numsStringsArray = req.query.nums.split(",");
+	if (!numsStringsArray) {
+		throw new AppError('Error: query "nums" must be present', 400);
+	}
+	return numsStringsArray;
+};
+
+module.exports = { Nums, parse, getArray };
---
+++ nums.test.js
@@ -0,0 +1,41 @@
+const { Nums, parse, getArray } = require("./nums");
+const { AppError } = require("./appError");
+
+describe("mean function", () => {
+	test("should return mean for valid data", () => {
+		const arr = [1, 2, 6, 9, 3, 4, 6];
+		const set = new Nums(arr);
+		expect(set.mean).toEqual(4.4286);
+	});
+});
+
+describe("median function", () => {
+	test("should return median for valid data", () => {
+		const arr = [1, 2, 6, 9, 3, 4, 6];
+		const set = new Nums(arr);
+		expect(set.median).toEqual(4);
+	});
+});
+
+describe("mode function", () => {
+	test("should return mode for valid data", () => {
+		const arr = [1, 2, 6, 9, 3, 4, 6];
+		const set = new Nums(arr);
+		expect(set.mode).toEqual(6);
+	});
+});
+
+describe("parse function", () => {
+	test("should retun valid array of integers if all are parse-able", () => {
+		const arr = ["2", "3", "1", "8"];
+		const parsedArray = parse(arr);
+		expect(parsedArray).toEqual([2, 3, 1, 8]);
+	});
+
+	test("should return appError if one of the strings cannot be parsed to int", () => {
+		const arr = ["1", "foo", "3", "5"];
+		expect(() => {
+			parse(arr);
+		}).toThrow(AppError);
+	});
+});
